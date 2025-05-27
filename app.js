4826e2ef241fdde269c00ecd832f8ee5d6037750
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-05-27 20:55:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ scene.background = new THREE.Color(0x000000);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 30, 50);
+camera.lookAt(0, 0, 0); // fokus ke tengah
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('tronCanvas') });
 renderer.setSize(window.innerWidth, window.innerHeight);
